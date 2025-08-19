9189be7a8984a3151804eecd08cc3709c11e3f2e
abdylla adhiyasa <adhiyasaabdylla@gmail.com>
2025-08-19 21:26:46 +0700

    initial

---
+++ app/src/main/java/com/smktunas/app4_recycleview/DetailActivity.kt
@@ -0,0 +1,28 @@
+package com.smktunas.app4_recycleview
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_buku)
+
+        val tvJudul: TextView = findViewById(R.id.tvJudulDetail)
+        val tvPenulis: TextView = findViewById(R.id.tvPenulisDetail)
+        val tvTahun: TextView = findViewById(R.id.tvTahunDetail)
+        val ivCover: ImageView = findViewById(R.id.ivCoverDetail)
+
+        tvJudul.text = intent.getStringExtra("judul")
+        tvPenulis.text = intent.getStringExtra("penulis")
+        tvTahun.text = intent.getStringExtra("tahun")
+
+        Glide.with(this)
+            .load(intent.getStringExtra("cover"))
+            .into(ivCover)
+    }
+}
---
+++ app/src/main/java/com/smktunas/app4_recycleview/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.smktunas.app4_recycleview
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.smktunas.app4_recycleview.adapter.BukuAdapter
+import com.smktunas.app4_recycleview.model.Buku
+import com.smktunas.app4_recycleview.utils.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBuku: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvBuku = findViewById(R.id.rvBuku)
+        rvBuku.layoutManager = LinearLayoutManager(this)
+
+        RetrofitClient.instance.getBuku().enqueue(object : Callback<List<Buku>> {
+            override fun onResponse(call: Call<List<Buku>>, response: Response<List<Buku>>) {
+                if (response.isSuccessful) {
+                    rvBuku.adapter = BukuAdapter(response.body() ?: emptyList())
+                }
+            }
+
+            override fun onFailure(call: Call<List<Buku>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Gagal: ${t.message}", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+}
